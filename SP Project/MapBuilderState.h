2a1080f59331506eedfe3b8b43cc4d87a724dc10
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-03-26 23:46:28 +0200

    updates to map array system

--- SP Project/MapBuilderState.h
+++ SP Project/MapBuilderState.h
@@ -7,25 +7,34 @@ class MapBuilderState : public State
 {
 private:
 	//variables:
-	 int size_x, size_y;  //<-- map size
-	 int x = 0, y = 0;    //<-- location of upper left corner of the map
-	 int x_offset = 0, y_offset = 0; //<-- offset from upper left corner of the screen to upper left corner of the map
-	 int x_mid = 0, y_mid = 0;       //<-- index of the tile in the center of the screen
-	 int cam_speed = 150;
-	 bool active_grid = 1; //<-- grid active/inactive
-	 float scale = 10;
-	 float dt = 0;
-	 pair<char, char> Map[256][256];    //the level map
+	struct Tile {
+		Vector2i layers[3]{ {21,49}, {5,6}, {5,6} }; //<-- cords of texture in sprite sheet
+		bool blocked = 0, hitbox = 0;
+	};
+	struct tex_props {
+		bool blocked = 0, hitbox = 0; int layer = 0;
+	};
 
-	 //private functions:
-	 void grid(RenderWindow*, int, int);
+	int size_x, size_y;  //<-- map size
+	int x = 0, y = 0;    //<-- location of upper left corner of the map
+	int x_offset = 0, y_offset = 0; //<-- offset from upper left corner of the screen to upper left corner of the map
+	int x_mid = 0, y_mid = 0;       //<-- index of the tile in the center of the screen
+	int cam_speed = 150;
+	bool active_grid = 1; //<-- grid active/inactive
+	float scale = 10;
+	float dt = 0;
+	Tile tiles[256][256];    //the level map
+	tex_props props[96][96];
 
+	//private functions:
+	void grid(RenderWindow*, int, int);
+	void render_tiles(string, RenderWindow*, int, int, int);
 
 public:
 	//constructors/destructors:
-	MapBuilderState(int a = 64,int b = 64);
+	MapBuilderState(int a = 64, int b = 64);
 	~MapBuilderState();
-	
+
 	//public functions:
 	void update(float);
 	void render(RenderWindow*);
