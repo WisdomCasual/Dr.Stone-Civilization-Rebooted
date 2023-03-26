651aae15bbd503ce3774647b38031b07db14d3bb
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-03-26 23:46:28 +0200

    updates to map array system

--- SP Project/MapBuilderState.cpp
+++ SP Project/MapBuilderState.cpp
@@ -5,11 +5,6 @@ MapBuilderState::MapBuilderState(int a, int b) : size_x(a), size_y(b)
 {
 	//loads "game" textures
 	intial_textures("game");
-
-	//gives intial water texture to all tiles 
-	for (int i = 0; i < size_x; i++)
-		for (int j = 0; j < size_y; j++)
-			Map[i][j] = {21, 49};
 }
 
 MapBuilderState::~MapBuilderState()
@@ -36,16 +31,29 @@ void MapBuilderState::grid(RenderWindow* window, int x_win, int y_win)
 	}
 }
 
+void MapBuilderState::render_tiles(string Tex, RenderWindow* window, int x_win, int y_win, int layer)
+{
+	Sprite tile;
+	tile.setTexture(*textures[Tex]);
+	tile.setScale(scale, scale);
+	for (int i = (x_offset > 0) ? x_offset : 0; i < (x_win + ((x_offset + 1) * 16 * scale)) / (16 * scale) && i < size_x; i++)
+		for (int j = (y_offset > 0) ? y_offset : 0; j < (y_win + ((y_offset + 1) * 16 * scale)) / (16 * scale) && j < size_y; j++) {
+			tile.setTextureRect(IntRect(tiles[i][j].layers[layer].x * 16, tiles[i][j].layers[layer].y * 16, 16, 16));
+			tile.setPosition(x + (16 * scale * i), y + (16 * scale * j));
+			window->draw(tile);
+		}
+}
+
 void MapBuilderState::update(float dt)
 {
 	//moves the Camera, increase cam screen with "Left Ctrl"
 	cam_speed = 150; this->dt = dt;
 	if (Keyboard::isKeyPressed(Keyboard::LControl))
-		cam_speed=500;
+		cam_speed = 500;
 	Vector2f movement = delta_movement();
 	x -= dt * cam_speed * movement.x * scale;
 	y -= dt * cam_speed * movement.y * scale;
-	x_offset = - x / (scale * 16);
+	x_offset = -x / (scale * 16);
 	y_offset = -y / (scale * 16);
 
 }
@@ -55,18 +63,12 @@ void MapBuilderState::render(RenderWindow* window)
 	int x_win = window->getSize().x, y_win = window->getSize().y; //<-- window boundaries
 	x_mid = x_offset + (x_win / (16 * scale) / 2), y_mid = y_offset + (y_win / (16 * scale) / 2); // updates offset
 
-	//make as function//////////////////////
 	//renders the map in active screen area only
-	Sprite tile;
-	tile.setTexture(*textures["Outdoors_Spring"]);
-	tile.setScale(scale, scale);
-    for (int i = (x_offset>0)? x_offset : 0; i < (x_win+((x_offset+1)*16*scale)) / (16*scale) && i < size_x; i++)
-		for (int j = (y_offset > 0) ? y_offset : 0; j < (y_win + ((y_offset+1) * 16 * scale)) / (16 * scale) && j < size_y; j ++) {
-			tile.setTextureRect(IntRect(Map[i][j].first * 16, Map[i][j].second * 16, 16, 16));
-			tile.setPosition(x + (16 * scale * i), y + (16 * scale * j));
-			window->draw(tile);
-		}
-	////////////////////////
+	render_tiles("Outdoors_Spring", window, x_win, y_win, 0);
+	render_tiles("Outdoors_Spring", window, x_win, y_win, 1);
+	//render entities
+
+	render_tiles("Outdoors_Spring", window, x_win, y_win, 2);
 
 	grid(window, x_win, y_win);
 }
@@ -82,12 +84,12 @@ void MapBuilderState::pollevent(Event event, RenderWindow* window)
 			case Keyboard::Escape:
 				window->close(); break;
 
-			//activate deactivate grid lines
+				//activate deactivate grid lines
 			case Keyboard::G:
 				active_grid = !active_grid; break;
 			}
 
-		//camera zoom
+			//camera zoom
 		case Event::MouseWheelMoved:
 			if (event.type == Event::MouseWheelMoved)
 				if (scale + event.mouseWheel.delta > 1 && scale + event.mouseWheel.delta < 20) {
