b805942d87013050cb2bee90643622ba969b7697
CodingPanda166 <as225@DESKTOP-SHNPOFF>
2023-03-29 21:37:54 +0200

    complete SavesState

--- SP Project/SavesState.h
+++ SP Project/SavesState.h
@@ -7,13 +7,20 @@ class SavesState :
 private:
 	//variables:
 	float scale = 1;
-	bool save_intersection[3] = {};
+	bool save_empty[3] = { 0,0,0 };
 	Sprite save[3], arrow, savesBG;
+	 Text Delete [3];
+	 bool del_presse[3] = { 0,0,0 };
+	 int x_win, y_win;
+	 float pos_x, pos_y, dis;
 	//private functions:
 	void render_save(RenderWindow*,int,int,int,Font);
 	void render_arrow(RenderWindow*);
 	void render_savesBG(RenderWindow*,Font);
-
+	void update_save(RenderWindow*, int,int,int);
+	void update_arrow(RenderWindow*);
+	void draw_text(RenderWindow* ,string, int, int, int,Font);
+	void draw_level(RenderWindow*, string, int, int, int, Font);
 public:
 	//constructors/destructors:
 	SavesState();
