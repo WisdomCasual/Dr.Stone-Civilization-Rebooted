88b8987425505d4021fab7e2c8d179cfb540932d
Sheikh Chinese <2022170504@cis.asu.edu.eg>
2023-03-29 23:35:24 +0200

    ..

--- SP Project/SettingsState.cpp
+++ SP Project/SettingsState.cpp
@@ -2,6 +2,7 @@
 
 SettingsState::SettingsState()
 {
+	initial_fps();
 	initial_textures("settings");
 	tissue.setTexture(*textures.at(0));
 	tissue.setTextureRect(buttons[3]);
@@ -12,20 +13,36 @@ SettingsState::~SettingsState()
 
 }
 
-void SettingsState::update(float, RenderWindow*, int*, deque<State*>*)
+void SettingsState::update(float dt, RenderWindow* window, int* terminator, deque<State*>* states)
 {
-
+	if (fps_active)
+		calc_fps(dt);
 }
 
 
 
 void SettingsState::render(RenderWindow* window)
 {
+
 	window->draw(tissue);
+	if (fps_active)
+		window->draw(fps_text);
 }
 
 void SettingsState::pollevent(Event event, RenderWindow* window)
 {
-
+	while (window->pollEvent(event)) {
+		switch (event.type) {
+		case Event::Closed:
+			window->close(); break;
+		case Event::KeyPressed:
+			switch (event.key.code) {
+			case Keyboard::Escape:
+				window->close(); break;
+			case Keyboard::F3:
+				fps_active = !fps_active; break;
+			}
+		}
+	}
 
 }
