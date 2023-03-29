8b225450a01ddc275cdc40e529a5131e70fe6e63
Sheikh Chinese <2022170504@cis.asu.edu.eg>
2023-03-30 00:02:25 +0200

    Added Tint in background

--- SP Project/SettingsState.cpp
+++ SP Project/SettingsState.cpp
@@ -6,6 +6,10 @@ SettingsState::SettingsState()
 	initial_textures("settings");
 	tissue.setTexture(*textures.at(0));
 	tissue.setTextureRect(buttons[3]);
+	tissue.setOrigin(92 / 2, 92 / 2);
+	tint.setSize({1920, 1080});
+	tint.setFillColor(Color(0,0,0,154));
+	
 }
 
 SettingsState::~SettingsState()
@@ -15,6 +19,17 @@ SettingsState::~SettingsState()
 
 void SettingsState::update(float dt, RenderWindow* window, int* terminator, deque<State*>* states)
 {
+	win_x = window->getSize().x, win_y = window->getSize().y;
+	x = win_x / 2, y = win_y / 2;
+	if (win_x / 120.0 < win_y / 120.0) scale = win_x / 120.0;
+	else scale = win_y / 120.0;
+	/////// Do Not touch;
+
+	tint.setSize({ win_x, win_y });
+
+	tissue.setPosition(x, y);
+	tissue.setScale(scale, scale);
+
 	if (fps_active)
 		calc_fps(dt);
 }
@@ -23,7 +38,7 @@ void SettingsState::update(float dt, RenderWindow* window, int* terminator, dequ
 
 void SettingsState::render(RenderWindow* window)
 {
-
+	window->draw(tint);
 	window->draw(tissue);
 	if (fps_active)
 		window->draw(fps_text);
--- SP Project/SettingsState.h
+++ SP Project/SettingsState.h
@@ -11,11 +11,13 @@ private:
 		tipsleft[4] = { {370,126,9,18} ,{370,90,9,18} ,{372,294,9,18},{ 372,330,9,18 } },
 		mids[4] = { {338,449,18,18} ,{356,368,18,18} ,{356,431,18,18},{338,386,18,18} },
 		arrow = { 303,486,22,21 },
-		buttons[4] = { {290,139,93,94} ,{240,0,93,94} ,{290,49,93,94},{201,296,93,94} },
+		buttons[4] = { {290,139,93,94} ,{240,0,93,94} ,{290,49,93,94},{201,296,92,92} },
 		checkMark = { 370,45,16,15 };
 	//Colors loaded in arrangment : Yellow -> Red -> Blue
 	Sprite tipleft, tipright, mid,tissue;
-
+	RectangleShape tint;
+	float x = 0, y = 0;
+	float win_x = 0, win_y = 0,scale = 1;
 public:
 	//constructors / destructors
 	SettingsState();
