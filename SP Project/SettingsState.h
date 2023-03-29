d3042584187e5d9e9db62ad7bff19de22d543426
Sheikh Chinese <2022170504@cis.asu.edu.eg>
2023-03-29 22:56:59 +0200

    Setting state in progress 40%

---
+++ SP Project/SettingsState.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "State.h"
+using namespace std;
+using namespace sf;
+
+class SettingsState :
+	public State
+{
+private:
+	IntRect tipsright[4] = { {190,330,9,18} ,{190,348,9,18} ,{372,312,9,18}, { 190,294,9,18 } },
+		tipsleft[4] = { {370,126,9,18} ,{370,90,9,18} ,{372,294,9,18},{ 372,330,9,18 } },
+		mids[4] = { {338,449,18,18} ,{356,368,18,18} ,{356,431,18,18},{338,386,18,18} },
+		arrow = { 303,486,22,21 },
+		buttons[4] = { {290,139,93,94} ,{240,0,93,94} ,{290,49,93,94},{201,296,93,94} },
+		checkMark = { 370,45,16,15 };
+	//Colors loaded in arrangment : Yellow -> Red -> Blue
+	Sprite tipleft, tipright, mid,tissue;
+
+public:
+	//constructors / destructors
+	SettingsState();
+
+	~SettingsState();
+
+	void update(float);
+
+	void pollevent(Event, RenderWindow*);
+
+	void render(RenderWindow*);
+};
