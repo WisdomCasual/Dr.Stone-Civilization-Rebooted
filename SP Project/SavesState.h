2b2068e04fe1d561591ce3f5f7d29f9b358e6c21
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-03-30 06:41:58 +0200

    save state optimization

--- SP Project/SavesState.h
+++ SP Project/SavesState.h
@@ -8,7 +8,7 @@ private:
 	//variables:
 	struct save {
 		int x, y, no;
-		bool& excute;
+		int progress = 0, character_id = 0;
 		string name;
 		bool empty = 1, hover = 0, del_hover = 0, pressed = 0, del_pressed = 0, del = 0;
 	};
@@ -16,12 +16,10 @@ private:
 
 	Text text, del;
     float scale = 1;
-	bool save1 = 0, save2 = 0, save3 = 0;
-	save saves[3] = { {-130,0,1,save1}, {0,0,2,save2}, {130,0,3,save3} };
+	save saves[3] = { {-130,0,1}, {0,0,2}, {130,0,3} };
 
 
-	bool save_empty[3] = { 0,0,0 };
-	Sprite save_bg, save[3], arrow, savesBG;
+	Sprite save_bg, savesBG, charactertex, arrow;
 	Text Delete[3];
 	bool del_presse[3] = { 0,0,0 };
 	
@@ -33,18 +31,12 @@ private:
 	float pos_x = 0, pos_y = 0, dis = 0;
 
 	//private functions:
+	void initial_saves();
 	void update_saves(RenderWindow*);
 	void render_saves(RenderWindow*);
 
-
-
-	void render_save(RenderWindow*, int, int, int, Font);
-	void render_arrow(RenderWindow*);
-	void render_savesBG(RenderWindow*, Font);
-	void update_save(RenderWindow*, int);
-	void update_arrow(RenderWindow*);
 	void draw_text(RenderWindow*, string, int, int, int);
-	void draw_level(RenderWindow*, string, int, int, int, Font);
+
 public:
 	//constructors/destructors:
 	SavesState();
