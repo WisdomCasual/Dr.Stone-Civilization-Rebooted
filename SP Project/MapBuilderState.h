8358050aaf2ad12023d1d321b61ec29858aff932
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-03-30 08:36:15 +0200

    added selection in tile picker

--- SP Project/MapBuilderState.h
+++ SP Project/MapBuilderState.h
@@ -26,7 +26,7 @@ private:
 	float scale = 10;
 	float dt = 0;
 	int layer = 0;
-	bool blocked = 0, hitbox = 0;
+	bool blocked = 0, hitbox = 0, drawn_selection = 0;
 	Vector2f mouse_pos = { 0, 0 }, relative_mouse_pos = {0, 0}, hover_tile = { 0 , 0 };
 	Vector2i selected_tile = { 0, 0 }; 
 	Text info;
