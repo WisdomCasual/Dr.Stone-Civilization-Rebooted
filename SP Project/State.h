e388174d45fa3b24155065d4fb78feb7d16e04a8
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-03-31 05:06:37 +0200

    moved draw text function to "public state" to be usable across all states

--- SP Project/State.h
+++ SP Project/State.h
@@ -29,7 +29,7 @@ public:
 		int start_x, start_y, wdth = 0, hght = 0;
 	};
 	Font font;
-	Text fps_text;
+	Text fps_text, text;
 	float delay = 0;
 	int fps = 0, frame_count = 0, frame_sum = 0;
 	bool fps_active = 0;
@@ -38,6 +38,8 @@ public:
 	void initial_textures(string);
 	void initial_fps();
 	void calc_fps(float);
+	void draw_text(RenderWindow*, string, int, int, int);  //<-- gets window pointer, string, X cord, Y cords, Character size to draw text
+
 
 	virtual void pollevent(Event, RenderWindow*) = 0;
 	virtual void update(float, RenderWindow*, int*, deque<State*>*) = 0;
