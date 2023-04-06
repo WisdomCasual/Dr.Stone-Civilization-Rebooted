5a2695908409d6ffeb863a4479b9d4dd839a7423
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-04-06 22:00:35 +0200

    Revert "fixed sliders & reset bug(s) and now apply changes"
    This reverts commit 2e2bd6dbae1311e59797c18fd964abde010b6a7a.

--- SP Project/SettingsState.h
+++ SP Project/SettingsState.h
@@ -2,10 +2,6 @@
 #include "State.h"
 #include"MainMenuState.h"
 #include"MapBuilderState.h"
-#include "Game.h"
-
-#define resnum 10
-#define framelimnum 6
 
 using namespace std;
 using namespace sf;
@@ -27,6 +23,9 @@ private:
     RectangleShape tint;
     float x = 0, y = 0;
     float win_x = 0, win_y = 0, scale = 1, previous_scale = 1;
+    short int resolution = 4, framelimit = 3, game_volume = 100, music_volume = 100;
+    const Vector2u resolutions[10] = { {800, 600}, {1280, 720}, {1280, 800}, {1366, 768}, {1920, 1080}, {1920, 1200}, {2560, 1440}, {2560, 1600}, {3840, 2160}, {3840, 2400} };
+    const short int framelimits[6] = { 30, 60, 90, 120, 144, 165 };
     bool button_pressed = 0, arrow_pressed = 0;
     Vector2f mouse_pos = { 0,0 };
     Sprite tip;
@@ -35,32 +34,24 @@ private:
     int save_framelimit = 120, save_volume = 100, save_music = 100;
     bool save_vsync = 0;
 
-    short int resolution = resnum, framelimit = framelimnum, game_volume = 100, music_volume = 100;
-    VideoMode resolutions[resnum + 1] = { {800, 600}, {1280, 720}, {1280, 800}, {1366, 768}, {1920, 1080}, {1920, 1200}, {2560, 1440}, {2560, 1600}, {3840, 2160}, {3840, 2400} };
-    short int framelimits[framelimnum + 1] = { 30, 60, 90, 120, 144, 165 }; //<-- last slot reserved for custom paramaters ^
-    string resolutions_text[resnum + 1];
-    string framelimits_text[framelimnum + 1];
-    
 
 
     struct slider_info {
         short int x = 0, y = 0, mx = 1, color = 0;
         short int* linker = nullptr;
         string name = "";
-        char text_type = 0;     //0 = plain text, 1 = percentage (a%), 2 = custom (link string array)
-        string* txt = nullptr;
+        char text_type = 0;     //0 = plain text, 1 = percentage (a%), 2 = pair (a x b)
         short int tipx = 0;
         float midscale = 1;
         bool presssed = 0;
 
-    }sliders[4] = { {-105, -15, resnum-1, 0, &resolution, "Resolution", 2, resolutions_text}, {-105, 0, framelimnum-1, 1, &framelimit, "Frame Limit", 2, framelimits_text}, {-105, 15, 100, 2, &game_volume, "Game Sounds", 1}, {-105, 30, 100, 2, &music_volume, "Music", 1}};
+    }sliders[4] = { {-105, -15, 9, 0, &resolution, "Resolution", 2}, {-105, 0, 5, 1, &framelimit, "Frame Limit", 0}, {-105, 15, 100, 2, &game_volume, "Game Sounds", 1}, {-105, 30, 100, 2, &music_volume, "Music", 1} };
 
     //private functions:
     void update_arrow(RenderWindow*, int*, deque<State*>*);
     void dev_button(RenderWindow*, int*, deque<State*>*);
     void update_slider(RenderWindow*, slider_info*, int);
     void render_slider(RenderWindow*, int);
-    void settings_intializer();
 public:
     //constructors / destructors
     SettingsState();
