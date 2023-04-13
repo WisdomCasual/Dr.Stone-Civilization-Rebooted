b8e3c5c5d0593b34236eac60347417df925685d3
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-04-13 23:56:00 +0200

    WorldMap mostly complete

--- SP Project/WorldMapState.h
+++ SP Project/WorldMapState.h
@@ -12,15 +12,17 @@ struct WorldMapState : public State
 
 		struct Pin {
 			int x = 0, y = 0;
-			string name = "Antarctica";
-			bool pressed = 0;
-		}test;
+			float pin_scale = 2, namebox_scale = 1, text_size = 40;
+			bool pressed = 0, hover = 0;
+		};
 
+		map<string, Pin> pins;
 
 		RectangleShape tint;
 		float x = 0, y = 0;
-		float win_x = 0, win_y = 0, scale = 1, previous_scale = 1, text_size = 40;
-		bool admin = 0;
+		float win_x = 0, win_y = 0, scale = 1, previous_scale = 1;
+		bool admin = 0, new_map = 0, map_added = 0, del = 0, move = 0, selected = 0;
+		string moving;
 		Sprite worldmap, namebox, pin;
 		Vector2f mouse_pos = { 0,0 }, clicked_on = { -1, -1 };;
 		Vector2u prev_win = { 0, 0 };
@@ -28,6 +30,8 @@ struct WorldMapState : public State
 		//private functions
 		void update_pins();
 		void render_pins();
+		void save_maps();
+		void load_maps();
 
 
 	public:
