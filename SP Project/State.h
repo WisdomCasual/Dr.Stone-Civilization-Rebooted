4087afcb3fabc5f03715ec9fc938580d275cd659
joonyou <yaheahaha@gmail.com>
2023-04-13 20:31:06 +0200

    text box (WIP)

--- SP Project/State.h
+++ SP Project/State.h
@@ -10,6 +10,7 @@
 using namespace std;
 using namespace sf;
 
+inline Event text_event;
 struct State
 {
 private:
@@ -29,6 +30,29 @@ public:
 			     previous_drawn_tile = { -1,-1 }, previous_erased_tile = { -1,-1 };;
 		int start_x, start_y, wdth = 0, hght = 0;
 		int global_layer = 0, global_priority = 0;
+	};
+
+	struct text_box {
+		string input_string = "";
+		bool isActive = 0;
+		Text inputted_text;
+		RectangleShape box;
+
+		void text_poll(Event event) {
+			if (event.type == Event::MouseButtonPressed) {
+				
+			}
+			if (event.type == Event::TextEntered) {
+				if (event.text.unicode == 8 && !input_string.empty()) {
+					input_string.pop_back();
+					inputted_text.setString(input_string);
+				}
+				if (event.text.unicode > 31 && event.text.unicode < 127 && event.text.unicode != 96) {
+					input_string += event.text.unicode;
+					inputted_text.setString(input_string);
+				}
+			}
+		}
 	};
 
 	//public functions:
