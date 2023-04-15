95ed29a3c251f4270b585b8da936b9741331f332
Error1st <2022170086@cis.asu.edu.eg>
2023-04-15 11:12:30 +0200

    made some progress with NewSaveState

--- SP Project/NewSaveState.h
+++ SP Project/NewSaveState.h
@@ -13,17 +13,22 @@ struct NewSaveState :public State
 private:
 	//variables
 	string test_str = "";
+	RectangleShape tint;
 	TextBox txt_box;
 	RectangleShape black_box;
-	float scale = 1;
-	int win_x, win_y;
+	Sprite tissue, back_arrow;
+	bool arrow_pressed = 0;
+	float scale = 1, win_x=0, win_y=0, x=1, y=1;
+	Vector2f mouse_pos = { 0,0 }, clicked_on = { -1, -1 };
+	Vector2u prev_win = { 0, 0 };
 
+	//private functions
+	void update_arrow();
 public:
 	//constructors/destructors:
 	NewSaveState();
 	~NewSaveState();
 	//public functions:
-
 	void update();
 	void render();
 	void pollevent();
