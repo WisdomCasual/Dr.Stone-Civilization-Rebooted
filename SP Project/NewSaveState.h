0c771772a147a77a7cc57eff967d85319fc496a1
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-04-15 19:51:03 +0200

    bug fixes

--- SP Project/NewSaveState.h
+++ SP Project/NewSaveState.h
@@ -17,10 +17,11 @@ private:
 	TextBox txt_box;
 	RectangleShape black_box;
 	Sprite tissue, back_arrow;
-	bool arrow_pressed = 0;
+	bool arrow_pressed = 0, destruct = 0;
 	float scale = 1, win_x=0, win_y=0, x=1, y=1;
 	Vector2f mouse_pos = { 0,0 }, clicked_on = { -1, -1 };
 	Vector2u prev_win = { 0, 0 };
+
 
 	//private functions
 	void update_arrow();
