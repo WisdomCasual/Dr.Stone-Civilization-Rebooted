c4fc957f49cd14893af560b4d87f86d817c9aaa9
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-04-20 04:12:54 +0200

    notification optimizations

--- SP Project/NotificationState.cpp
+++ SP Project/NotificationState.cpp
@@ -1,21 +1,21 @@
 #include "NotificationState.h"
 void NotificationState::render_strings()
 {
-	y = notification_BG.getPosition().y;
-	text_y = y + 60 * (scale * .8) / 2.0;
+	y_pos = notification_BG.getPosition().y;
+	text_y = y_pos + 60 * (scale * .8) / 2.0;
 	
 	float i = (notification_lines / 2.0 - 2) * -1, dis = (92 / notification_lines) * scale*.9 ;
 	text.setFillColor(Color::Black);
 	for (int j = 0; j < notification_lines - 3; j++, i++) {
-		draw_text(notification_strings[j], text_x, text_y + i * dis, (3 + 75 / notification_lines) * scale);
+		draw_text(notification_strings[j], text_x, text_y + i * dis, (3 + 55 / notification_lines) * scale);
 	}
 
 }
 void NotificationState::update_velocity()
 {
 	float pos_y = notification_BG.getPosition().y;
-	if (pos_y>=win_y-60*scale || notification_delay>3)
-		velocity.y += g * dt*scale;
+	if (pos_y >= win_y - 60 * scale || notification_delay>3)
+		velocity.y += speed * dt * scale;
 	else {
 		notification_delay += dt;
 		velocity.y = 0;
@@ -32,11 +32,14 @@ void NotificationState::update_velocity()
 }
 NotificationState::NotificationState()
 {
+	scale = min(window->getSize().x / 350.0, window->getSize().y / 350.0);
+	if (window->getSize().x > 1280) scale *= 0.75;
 	initial_textures("notification");
 	notification_BG.setTexture(*textures[0]);
 	notification_BG.setOrigin(145,0);
-	x = window->getSize().x, y = window->getSize().y;
-	notification_BG.setPosition(x, y);
+	x_pos = window->getSize().x, y_pos = window->getSize().y;
+	notification_BG.setPosition(x_pos, y_pos);
+	velocity = { 0,(float) - 0.75 * scale};
 }
 
 NotificationState::~NotificationState()
@@ -54,18 +57,21 @@ void NotificationState::update()
 
 	if (prev_win != window->getSize()) {
 		prev_win = window->getSize();
+		y_pos /= scale;
 		win_x = window->getSize().x, win_y = window->getSize().y;
 		if (win_x / 350.0 < win_y / 350.0) scale = win_x / 350.0;
 		else scale = win_y / 350.0;
 		if (win_x > 1280) scale *= 0.75;
 		notification_BG.setScale(scale*.8, scale*.8);
-		x = win_x-10*scale;
-		text_x = x - 145 * (scale*.8) / 2.0;
-		text_y = y + 60 * (scale*.8) / 2.0;
-		notification_BG.setPosition(x, y);
+		x_pos = win_x-10*scale;
+		y_pos *= scale;
+		text_x = x_pos - 145 * (scale*.8) / 2.0;
+		text_y = y_pos + 60 * (scale*.8) / 2.0;
+		notification_BG.setPosition(x_pos, y_pos);
 	}
-	update_velocity();
 	prev(prev(states->end()))->second->update();
+	update_velocity();
+	//don't put anything after//
 }
 
 void NotificationState::render()
