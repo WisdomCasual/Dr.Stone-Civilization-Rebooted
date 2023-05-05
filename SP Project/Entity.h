2c8d36986ce7e6ad7e291d31b608670f6a73b62a
joonyou <yaheahaha@gmail.com>
2023-05-05 16:15:42 +0300

    Rays now cast

--- SP Project/Entity.h
+++ SP Project/Entity.h
@@ -39,7 +39,179 @@ private:
 	//////////////////7agat el darb//////////////////
 	FloatRect MakanElDarb;
 	Vector2f RangeElDarb={ 5,5 };
-	float Lag = 0, motion_delay = 0;
+	float Lag = 0, motion_delay = 4;
+	const float corners[2] = { 1, -1 };
+
+	struct comparison_tile {
+		float cost = 0, x = 0, y = 0;
+	};
+
+	struct path_tile {
+		float x = 0, y = 0;
+		path_tile* parent = nullptr;
+	};
+
+
+	struct path_array {
+
+		int size = 1, curr_idx = 0, layer = 0;
+		comparison_tile* at; //dynamic array of objects
+
+		path_array() {
+			at = new comparison_tile[1];
+		}
+
+		path_array(int size) {
+			at = new comparison_tile[size];
+			this->size = size;
+		}
+
+		~path_array() {
+			delete[] at;
+		}
+
+		void resize(const int size) {
+			comparison_tile* old_dynamic_array = at;
+			at = new comparison_tile[size];
+			int copy_size = min(curr_idx, size);
+			for (int i = 0; i < copy_size; i++)
+				at[i] = old_dynamic_array[i];
+			this->size = size;
+			delete[] old_dynamic_array;
+		}
+
+		void add(const comparison_tile new_object) {
+			if (curr_idx >= size)
+				resize(size * 2);
+
+			at[curr_idx] = new_object, curr_idx++;
+		}
+
+		int arr_size() {
+			return curr_idx;
+		}
+	};
+
+
+
+
+	struct comparison_array {
+
+		int size = 1, curr_idx = 0, layer = 0;
+		comparison_tile*at; //dynamic array of objects
+
+		comparison_array() {
+			at = new comparison_tile[1];
+		}
+
+		comparison_array(int size) {
+			at = new comparison_tile[size];
+			this->size = size;
+		}
+
+		~comparison_array() {
+			delete[] at;
+		}
+
+		void resize(const int size) {
+			comparison_tile* old_dynamic_array = at;
+			at = new comparison_tile[size];
+			int copy_size = min(curr_idx, size);
+			for (int i = 0; i < copy_size; i++)
+				at[i] = old_dynamic_array[i];
+			this->size = size;
+			delete[] old_dynamic_array;
+		}
+
+		void add(const comparison_tile new_object) {
+			if (curr_idx >= size)
+				resize(size * 2);
+
+			at[curr_idx] = new_object, curr_idx++;
+		}
+
+		int arr_size() {
+			return curr_idx;
+		}
+	};
+
+    struct tabor_el_3e4 {
+    private:
+
+
+        int size = 0;
+		comparison_array hp;
+    public:
+
+		tabor_el_3e4() {
+			hp.resize(128);
+		}
+        /* heap algorithm */
+        void sheftup(int indx) {
+            int parent = (indx - 1) / 2;
+            if (hp.at[parent].cost > hp.at[indx].cost) {
+                swap(hp.at[parent], hp.at[indx]);
+                sheftup(parent);
+            }
+        }
+        void sheftdown(int indx) {
+            int min_valu = indx, left = (indx * 2) + 1, right = (indx * 2) + 2;
+            if (left < size && hp.at[left].cost < hp.at[min_valu].cost)
+                min_valu = left;
+
+            if (right < size && hp.at[right].cost < hp.at[min_valu].cost)
+                min_valu = right;
+
+
+            if (min_valu != indx) {
+
+                swap(hp.at[min_valu], hp.at[indx]);
+                sheftdown(min_valu);
+
+            }
+        }
+
+
+        /* insert function */
+
+
+        void Ed5ol(comparison_tile dist) {
+            size++;
+			hp.add(dist);
+            sheftup(size - 1);
+        }
+
+        /* empty function */
+        bool Fare8() {
+            if (!size)
+                return 1;
+            else return 0;
+        }
+
+        /* delet top function */
+        void Astika() {
+            hp.at[0] = hp.at[size - 1];
+            size--;
+            sheftdown(0);
+        }
+
+        /* clear function */
+        void shinra_tensi() {
+            size = 0;
+        }
+
+
+
+		comparison_tile top() {
+            if (!Fare8()) {
+
+                return hp.at[0];
+            }
+            return comparison_tile(-1, -1, -1);
+        }
+    } a_star;
+
+
 public:
 	Entity(entity&, string, render_tile**&, float&, float&, Entity* player = nullptr);
 
@@ -50,6 +222,7 @@ public:
 	bool is_in_action();
 	void updatePos();
 	void change_state(int);
+	bool visionLines(Entity&);
 	bool entityFound(Entity&);
 	void setPlayerState(bool state); //1 = player, 0 = non-player
 	void setPosition(float x_pos, float y_pos);
