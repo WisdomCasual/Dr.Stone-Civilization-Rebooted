5363a11cf98db6c2afe6d3a3c7006f97cd892d64
Error1st <2022170086@cis.asu.edu.eg>
2023-05-08 02:46:01 +0300

    Lw Mawgood Hagebak

--- SP Project/Enemy.h
+++ SP Project/Enemy.h
@@ -9,12 +9,13 @@ private:
 	Vector2i prev_target_tile = { 0, 0 }, last_seen = { 0, 0 };
 	Vector2f target_tile = { -1.f, -1.f }, last_seen_cord = { 0.f, 0.f };
 	bool will_move = 0, prev_check = 0;
-	float theta = 0, motion_delay = 4, sound_range = 5.f;
+	float theta = 0, motion_delay = 4, sound_range = 5.f,RangeElWgood=90.0;
 	path_tile* mp = nullptr;
+
 
 public:
 	~Enemy();
-
+	void Mawgood();
 	void updatePos();
 	bool visionLines(Entity&);
 	bool entityFound(Entity&);
