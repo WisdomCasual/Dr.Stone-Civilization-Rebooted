6bdf78b8da2e07aa4fd6b32115a43e863aa8b28e
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-05-10 00:49:03 +0300

    animations (in progress)

--- SP Project/Animation.cpp
+++ SP Project/Animation.cpp
@@ -1,17 +1,48 @@
 #include "Animation.h"
 
-Animation::Animation()
+Animation::Animation(IntRect frame, int frame_count, Color texture_color, bool loop, float& map_x, float& map_y)
+	: map_x(map_x), map_y(map_y)
 {
+	this->frame = frame;
+	this->frame_count = frame_count;
+	this->loop = loop;
+
+	animation_sprite
+	animation_sprite.setOrigin(8, 8);
+
 }
 
 Animation::~Animation()
 {
+
 }
 
-void Animation::update()
+void Animation::update(float scale)
 {
+	if (prev_scale != scale) {
+		prev_scale = scale;
+		animation_sprite.setScale(scale, scale);
+	}
+
+	if (delay >= 200) {
+		delay = 0;
+		current_frame++;
+		if (current_frame >= frame_count) {
+			if (loop)
+				current_frame = 0;
+			else {
+				//self destruct
+			}
+
+		}
+	}
+	else
+		delay += dt;
+
+
 }
 
 void Animation::render()
 {
+	window->draw(animation_sprite);
 }
