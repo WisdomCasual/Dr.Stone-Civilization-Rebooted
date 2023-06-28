3ab3f117518dcc0262fcd1c83e9fb04d52f32230
joonyou <yaheahaha@gmail.com>
2023-06-28 07:24:07 +0300

    NPC interactions (WIP)

--- SP Project/NPC.cpp
+++ SP Project/NPC.cpp
@@ -58,17 +58,20 @@ bool NPC::legal_direction(Vector2f tile_pos, short dx, short dy)
 	return legal_tile(tile_pos, entity_stats.animations[state][move_num].hitbox_rect);
 }
 
-void NPC::stateMachine()
+void NPC::set_type(short type)
 {
-	switch (action_state) {
-	case 1: {
-		//dialogue behaviour?
-		break;
-	}
-	default:
-		//non-dialogue behaviour?
-		break;
-	}
+	npc_type = type;
+}
+
+void NPC::set_dialogue(dialogue*& dialogues, short n)
+{
+	dialogues_num = n;
+	if (npc_dialogues != nullptr)
+		delete[] npc_dialogues;
+	npc_dialogues = new dialogue[n];
+	for (int i = 0; i < n; i++)
+		npc_dialogues[i] = dialogues[i];
+	delete[] dialogues;
 }
 
 void NPC::update()
@@ -103,12 +106,17 @@ void NPC::update()
 	entity_sprite.setTextureRect(IntRect(current_rect.left + current_frame * current_rect.width, current_rect.top, current_rect.width, current_rect.height));
 	entity_sprite.setOrigin(entity_stats.animations[state][current_move].origin); ///////////////
 	updatePos();
-	stateMachine();
 
 	player_collision_check();
 	if (player_entity.interact && magnitude(player_entity.getRelativePos() - getRelativePos()) <= 32) {
 		player_entity.interact = 0;
-		cout << "HI!";
+		switch (npc_type) {
+			default: {
+				//single_dialogue[0] = npc_dialogues[rand() % dialogues_num];         crashes for now
+				//states->insert({ DialogueID,new DialogueState(single_dialogue,{win_x / 2,win_y / 2},scale / 2, 1) });
+				//states->at(DialogueID)->update();
+			}
+		}
 	}
 	if (will_move && !collide_with_player(dt * move_speed * curr_movement)) {
 		move(dt * move_speed * curr_movement);
