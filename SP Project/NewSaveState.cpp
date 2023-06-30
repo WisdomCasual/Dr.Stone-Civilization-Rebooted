0bd27c3815381471b5203201fb70962de1b2e13d
Kareem Hatem <Kareem_hatem102@outlook.com>
2023-06-30 21:37:45 +0300

    <-

--- SP Project/NewSaveState.cpp
+++ SP Project/NewSaveState.cpp
@@ -54,10 +54,10 @@ bool NewSaveState::black_out()
 {
 	if (blackining < 255) {
 		blacking_out = true;
-		if (blackining + 750 * dt > 255)
+		if (blackining + 500 * dt > 255)
 			blackining = 255;
 		else
-			blackining += 750 * dt;
+			blackining += 500 * dt;
 		blackscreen.setFillColor(Color(0, 0, 0, blackining));
 		return false;
 	}
